d7aec27799e20a82f5eea638bdd36cf3bdb5ea14
Taurylax <147067906+Taurylax@users.noreply.github.com>
2023-10-05 14:38:29 +0100

    Update maybe grid lol.py

--- maybe grid lol.py
+++ maybe grid lol.py
@@ -12,18 +12,18 @@ counter = 0
 
 # Loop to draw a pattern of diagonal lines
 for i in range(200):
-    x1 = 10
+    x1 = 30
     if counter % 2 == 0:
         # Calculate the starting y-coordinate for even rows
-        y1 = (i * ((10 * maths.sqrt(3)) / 3))
-        inc = ((10 * maths.sqrt(3)) / 3)
+        y1 = (i * ((30 * maths.sqrt(3)) / 3))
+        inc = ((30 * maths.sqrt(3)) / 3)
     else:
         # Calculate the starting y-coordinate for odd rows
-        inc = -((10 * maths.sqrt(3)) / 3)
+        inc = -((30 * maths.sqrt(3)) / 3)
     
     # Loop to draw diagonal lines in a row
     for z in range(100):
-        x2 = x1 + 10
+        x2 = x1 + 30
         y2 = y1 + inc
         
         # Create a line on the canvas from (x1, y1) to (x2, y2)
@@ -33,23 +33,23 @@ for i in range(200):
         y1 = y2
         
         # Toggle the inclination for the next line
-        if inc == ((10 * maths.sqrt(3)) / 3):
-            inc = -((10 * maths.sqrt(3)) / 3)
+        if inc == ((30 * maths.sqrt(3)) / 3):
+            inc = -((30 * maths.sqrt(3)) / 3)
         else:
-            inc = ((10 * maths.sqrt(3)) / 3)
+            inc = ((30 * maths.sqrt(3)) / 3)
     
     counter += 1
 
 # Initialize coordinates for drawing vertical lines
-x1 = 10
+x1 = 30
 y1 = 0
-y2 = 1000
+y2 = 3000
 
 # Loop to draw vertical lines
 for p in range(100):
     # Create a vertical line on the canvas from (x1, y1) to (x1, y2)
     line = canvas.create_line(x1, y1, x1, y2)
-    x1 += 10
+    x1 += 30
 
 # Pack the canvas widget into the window
 canvas.pack()
