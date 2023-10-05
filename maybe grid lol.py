c67ba5aa6852c5eb90d015433f655d1446b48bac
Josh Sawyer <144061186+JoshSawyer1@users.noreply.github.com>
2023-10-05 14:34:17 +0100

    IsometricGrid.py

--- maybe grid lol.py
+++ maybe grid lol.py
@@ -1,33 +1,58 @@
 import tkinter
 import math as maths
 
+# Create a Tkinter window
 window = tkinter.Tk()
+
+# Create a canvas widget with a white background and specified dimensions
 canvas = tkinter.Canvas(window, bg="white", height=1000, width=1000)
 
+# Initialize a counter variable
 counter = 0
+
+# Loop to draw a pattern of diagonal lines
 for i in range(200):
     x1 = 10
-    if counter%2 == 0:
-        y1 = (i*((10*maths.sqrt(3))/3))
-        inc = ((10*maths.sqrt(3))/3)
+    if counter % 2 == 0:
+        # Calculate the starting y-coordinate for even rows
+        y1 = (i * ((10 * maths.sqrt(3)) / 3))
+        inc = ((10 * maths.sqrt(3)) / 3)
     else:
-        inc = -((10*maths.sqrt(3))/3)
+        # Calculate the starting y-coordinate for odd rows
+        inc = -((10 * maths.sqrt(3)) / 3)
+    
+    # Loop to draw diagonal lines in a row
     for z in range(100):
         x2 = x1 + 10
         y2 = y1 + inc
+        
+        # Create a line on the canvas from (x1, y1) to (x2, y2)
         line = canvas.create_line(x1, y1, x2, y2)
+        
         x1 = x2
         y1 = y2
-        if inc == ((10*maths.sqrt(3))/3):
-            inc = -((10*maths.sqrt(3))/3)
+        
+        # Toggle the inclination for the next line
+        if inc == ((10 * maths.sqrt(3)) / 3):
+            inc = -((10 * maths.sqrt(3)) / 3)
         else:
-            inc = ((10*maths.sqrt(3))/3)
+            inc = ((10 * maths.sqrt(3)) / 3)
+    
     counter += 1
 
+# Initialize coordinates for drawing vertical lines
 x1 = 10
 y1 = 0
 y2 = 1000
+
+# Loop to draw vertical lines
 for p in range(100):
-    line = canvas.create_line(x1,y1,x1,y2)
+    # Create a vertical line on the canvas from (x1, y1) to (x1, y2)
+    line = canvas.create_line(x1, y1, x1, y2)
     x1 += 10
+
+# Pack the canvas widget into the window
 canvas.pack()
+
+# Start the Tkinter main loop
+window.mainloop()
