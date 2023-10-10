25c556cdb1d6933aac6a8a72a685701f4b7ea035
GoofyGoober6969 <147066972+GoofyGoober6969@users.noreply.github.com>
2023-10-10 16:16:38 +0100

    Update App.py
    Added placement for frogs in a grid as they will be to solve the puzzle, frogs are identical for now, placement logic still needs to be added.

--- App.py
+++ App.py
@@ -1,5 +1,4 @@
 # Board
-
 import tkinter
 import math as maths
 import random
@@ -8,7 +7,7 @@ import random
 window = tkinter.Tk()
 
 # Create a canvas widget with a white background and specified dimensions
-canvas = tkinter.Canvas(window, bg="black", height=1000, width=1500)
+canvas = tkinter.Canvas(window, bg="black", height=10000, width=15000)
 
 # Initialize a counter variable
 counter = 0
@@ -18,7 +17,7 @@ colours = ["sky blue","red","green","orange","white","yellow"]
 vertex_coordinates = []
 
 # Loop to draw a pattern of diagonal lines
-for i in range(66):
+for i in range(100):
     x1 = 0
     if counter % 2 == 0:
         # Calculate the starting y-coordinate for even rows
@@ -29,11 +28,11 @@ for i in range(66):
         inc = -((27 * maths.sqrt(3)) / 3)
 
     # Loop to draw diagonal lines in a row
-    for z in range(58):
+    for z in range(100):
         x2 = x1 + 27
         y2 = y1 + inc
         colour = random.choice(colours)
-        
+       
         # Create a line on the canvas from (x1, y1) to (x2, y2)
         line = canvas.create_line(x1, y1, x2, y2, fill=colour)
 
@@ -58,7 +57,7 @@ y1 = 0
 y2 = 0
 
 # Loop to draw vertical lines
-for p in range(56):
+for p in range(100):
     # Calculate the coordinates of the top and bottom vertices
     x2 = x1
     y2 = y1 + 2700
@@ -68,16 +67,27 @@ for p in range(56):
 
     x1 += 27
 
-# Creating / colouring frog shape on board
-x = 27
-y = 27*maths.sqrt(3)
-left_leg = canvas.create_polygon(x,y,x+81,y+(27*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),x,y+(18*maths.sqrt(3)),x,y, fill=random.choice(colours))
-right_leg = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+81,y+(63*maths.sqrt(3)),x,y+(90*maths.sqrt(3)),x,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)), fill=random.choice(colours))
-right_torso = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+81,y+(63*maths.sqrt(3)),x+135,y+(81*maths.sqrt(3)),x+135,y+(99*maths.sqrt(3)),x+162,y+(108*maths.sqrt(3)),x+162,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)), fill=random.choice(colours))
-left_torso = canvas.create_polygon(x+81,y+(27*maths.sqrt(3)),x+135,y+(9*maths.sqrt(3)),x+135,y-(9*maths.sqrt(3)),x+162,y-(18*maths.sqrt(3)),x+162,y,x+162,y+(18*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),x+81,y+(27*maths.sqrt(3)), fill= random.choice(colours))
-head = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+162,y+(18*maths.sqrt(3)),x+243,y+(45*maths.sqrt(3)),x+162,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),fill = random.choice(colours))
-left_arm = canvas.create_polygon(x+162,y-(18*maths.sqrt(3)),x+216,y,x+216,y+(18*maths.sqrt(3)),x+162,y,fill = random.choice(colours))
-right_arm = canvas.create_polygon(x+162,y+(108*maths.sqrt(3)),x+216, y+(90*maths.sqrt(3)),x+216, y+(72*maths.sqrt(3)),x+162, y+(90*maths.sqrt(3)),fill = random.choice(colours))
+# Create the loops for placing frogs and make starting variables
+counter = 1
+x_start = 27
+y = 18*maths.sqrt(3)
+y_start = 18*maths.sqrt(3)
+for i in range(9):
+    if counter%2 == 0:
+        x = x_start
+    else:
+        x = x_start+81
+    for z in range(9):
+        left_leg = canvas.create_polygon(x,y,x+81,y+(27*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),x,y+(18*maths.sqrt(3)),x,y, fill="white")
+        right_leg = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+81,y+(63*maths.sqrt(3)),x,y+(90*maths.sqrt(3)),x,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)), fill="red")
+        right_torso = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+81,y+(63*maths.sqrt(3)),x+135,y+(81*maths.sqrt(3)),x+135,y+(99*maths.sqrt(3)),x+162,y+(108*maths.sqrt(3)),x+162,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)), fill="orange")
+        left_torso = canvas.create_polygon(x+81,y+(27*maths.sqrt(3)),x+135,y+(9*maths.sqrt(3)),x+135,y-(9*maths.sqrt(3)),x+162,y-(18*maths.sqrt(3)),x+162,y,x+162,y+(18*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),x+81,y+(27*maths.sqrt(3)), fill="green")
+        head = canvas.create_polygon(x+81,y+(45*maths.sqrt(3)),x+162,y+(18*maths.sqrt(3)),x+243,y+(45*maths.sqrt(3)),x+162,y+(72*maths.sqrt(3)),x+81,y+(45*maths.sqrt(3)),fill="blue")
+        left_arm = canvas.create_polygon(x+162,y-(18*maths.sqrt(3)),x+216,y,x+216,y+(18*maths.sqrt(3)),x+162,y,fill="yellow")
+        right_arm = canvas.create_polygon(x+162,y+(108*maths.sqrt(3)),x+216, y+(90*maths.sqrt(3)),x+216, y+(72*maths.sqrt(3)),x+162, y+(90*maths.sqrt(3)),fill="white")
+        x = x + 162
+    y = y_start+((i+1)*81*maths.sqrt(3))
+    counter += 1
 
 # Pack the canvas widget into the window
 canvas.pack()
