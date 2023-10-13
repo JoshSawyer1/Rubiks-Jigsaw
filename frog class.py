8d294e3bc2b14b7c585140505214d228ab6eba70
CasualyDying <147068082+CasualyDying@users.noreply.github.com>
2023-10-13 14:19:09 +0100

    Add files via upload

---
+++ frog class.py
@@ -0,0 +1,33 @@
+# Frog class
+# Randomise frogs + colours
+import random
+
+class frog:
+    # Class variables
+    isFrog = True
+    colours = ("white", "yellow", "red", "green", "blue", "oragne")
+
+    # Def function for frogs, not same frog. sets 'self' for frogs.
+    def __init__(self, RArm, LArm, RTorso, LTorso, RLeg, LLeg, Head):
+        self.RArm = right_arm
+        self.LArm = left_arm
+        self.RTorso = right_torso
+        self.LTorso = left_torso
+        self.RLeg = right_leg
+        self.LLeg = left_leg
+        self.Head = head
+        self.colourList = (right_arm, left_arm, right_torso, left_torso, right_leg, left_leg, head)
+
+    # Moves colours into sections of frog
+    def colourFrog(self):
+        self.RArm = self.colourList[0]
+        self.LArm = self.colourList[1]
+        self.RTorso = self.colourList[2]
+        self.LTorso = self.colourList[3]
+        self.RLeg = self.colourList[4]
+        self.LLeg = self.colourList[5]
+        self.Head = self.colourList[6]
+
+    # Random colours
+    def randomColourFrog(self):
+        
